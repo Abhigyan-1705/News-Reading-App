05aa2d6e23e8bae02c016ba99aa7d47303e923ca
Abhigyan-1705 <abhigyank175@gmail.com>
2026-06-09 23:14:15 +0530

    Added Home Page UI

--- app/src/main/java/com/example/myapplication/HomePage.kt
+++ app/src/main/java/com/example/myapplication/HomePage.kt
@@ -1,8 +1,53 @@
 package com.example.myapplication
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.kwabenaberko.newsapilib.models.Article
 
 @Composable
 fun HomePage(newsViewModel: NewsViewModel) {
+    val articles by newsViewModel.articles.observeAsState(emptyList())
 
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(articles) { article ->
+                ArticleItem(article)
+            }
+        }
+    }
+}
+
+@Composable
+fun ArticleItem(article: Article) {
+    Card(
+        modifier = Modifier.padding(8.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = article.title ?: "No Title")
+        }
+    }
 }
