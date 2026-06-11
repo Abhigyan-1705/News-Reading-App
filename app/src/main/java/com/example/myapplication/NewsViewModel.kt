842d4b353c2093f9fb6f1dc36dc38c10438e7033
Abhigyan-1705 <abhigyank175@gmail.com>
2026-06-11 17:47:39 +0530

    Added the Search Feature

--- app/src/main/java/com/example/myapplication/NewsViewModel.kt
+++ app/src/main/java/com/example/myapplication/NewsViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.kwabenaberko.newsapilib.NewsApiClient
 import com.kwabenaberko.newsapilib.models.Article
+import com.kwabenaberko.newsapilib.models.request.EverythingRequest
 import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest
 import com.kwabenaberko.newsapilib.models.response.ArticleResponse
 
@@ -25,6 +26,25 @@ class NewsViewModel : ViewModel() {
             .build()
 
         newsApiClient.getTopHeadlines(request, object : NewsApiClient.ArticlesResponseCallback {
+            override fun onSuccess(response: ArticleResponse?) {
+                response?.articles?.let {
+                    _articles.postValue(it)
+                }
+            }
+
+            override fun onFailure(throwable: Throwable?) {
+                Log.e("News API Response", "Request failed: ${throwable?.message}")
+            }
+        })
+    }
+    fun fetchEverythingWithQuery(query:String) {
+        val newsApiClient = NewsApiClient(Constant.API_KEY)
+        val request = EverythingRequest.Builder()
+            .language("en")
+            .q(query)
+            .build()
+
+        newsApiClient.getEverything(request, object : NewsApiClient.ArticlesResponseCallback {
             override fun onSuccess(response: ArticleResponse?) {
                 response?.articles?.let {
                     _articles.postValue(it)
